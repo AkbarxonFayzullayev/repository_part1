57863610987fa82d5744829198d27a5854b73de7
Akbar <akbarxonfayzullayev123@gmail.com>
2024-12-09 19:30:24 +0500

    Admin1

--- admin_1.py
+++ admin_1.py
@@ -1,4 +1,4 @@
-class Person:
+class Student:
     def __init__(self,name,phone):
         self.name = name
         self.phone = phone
--- test2.py
+++
@@ -1,6 +0,0 @@
-from test import Person
-
-class Student(Person):
-    def __init__(self,name,phone,adres):
-        super().__init__(name,phone)
-        self.adres = adres
