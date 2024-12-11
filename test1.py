d7b78a7451d89e80b43a4e311ce382a38f099f0f
AkbarxonFayzullayev <akbarxonfayzullayev555@gmail.com>
2024-12-11 19:28:37 +0500

    Master11 tahrirlandi

--- test1.py
+++ test1.py
@@ -4,3 +4,9 @@ class Student:
         self.phone = phone
     def info(self):
         print(f"Student ismi: {self.name}, Telefon raqami: {self.phone}")
+    def get_name(self):
+        print(self.name)
+
+
+student = Student("Abdyazim","+998909119091")
+student.get_name()
